54907396c995bd689b128401343a7e5290dcd189
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-20 15:08:41 +0300

    First commit in git(JAVA Advanced)

---
+++ src/Exercise_1_StacksAndQueues/P03_MaximumElement.java
@@ -0,0 +1,36 @@
+package Exercise_1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P03_MaximumElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            int[] command = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int commandType = command[0];
+
+            switch (commandType) {
+                case 1:
+                    stack.push(command[1]);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    System.out.println(Collections.max(stack));
+                    break;
+            }
+
+
+        }
+    }
+}
