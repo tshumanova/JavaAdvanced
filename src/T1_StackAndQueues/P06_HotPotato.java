2bc96b865ef56fb508e71f177564257343f0e19b
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-22 17:06:43 +0300

    Create HotPotato.java

--- src/T1_StackAndQueues/P06_HotPotato.java
+++ src/T1_StackAndQueues/P06_HotPotato.java
@@ -1,11 +1,16 @@
 package T1_StackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class P06_HotPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String[] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> kids = new ArrayDeque<>();
+        Collections.addAll(kids, input);
+        int number = Integer.parseInt(scanner.nextLine());
     }
 }
