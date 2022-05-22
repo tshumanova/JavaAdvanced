b619a8e170f4d3680a5af37d9fc67833e604b7f4
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-22 17:04:41 +0300

    Create HotPotato.java

---
+++ src/T1_StackAndQueues/P06_HotPotato.java
@@ -0,0 +1,11 @@
+package T1_StackAndQueues;
+
+import java.util.Scanner;
+
+public class P06_HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
