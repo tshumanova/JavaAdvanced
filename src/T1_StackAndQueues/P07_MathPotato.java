9db81b111b19b2cd19e6ad3cedb0787fdfddc4d9
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-23 16:31:25 +0300

    Create MathPotato.java

--- src/T1_StackAndQueues/P07_MathPotato.java
+++ src/T1_StackAndQueues/P07_MathPotato.java
@@ -1,6 +1,7 @@
 package T1_StackAndQueues;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class P07_MathPotato {
@@ -9,5 +10,15 @@ public class P07_MathPotato {
 
         String[] input = scanner.nextLine().split("\\s+");
         ArrayDeque<String> kids = new ArrayDeque<>();
+        Collections.addAll(kids, input);
+        int number = Integer.parseInt(scanner.nextLine());
+        int primeCircle = 1;
+        while (kids.size() > 1) {
+            for (int i = 1; i < number; i++) {
+                kids.offer(kids.poll());
+
+            }
+        }
+
     }
 }
