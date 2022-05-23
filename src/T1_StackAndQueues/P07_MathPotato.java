431a7e02328fd640a9136bf0159e6483c735b671
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-23 16:28:13 +0300

    Create MathPotato.java

--- src/T1_StackAndQueues/P07_MathPotato.java
+++ src/T1_StackAndQueues/P07_MathPotato.java
@@ -1,11 +1,13 @@
 package T1_StackAndQueues;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class P07_MathPotato {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String[] input = scanner.nextLine().split("\\s+");
+        ArrayDeque<String> kids = new ArrayDeque<>();
     }
 }
