55216369989641f25db921facd3657d6ef0e6648
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-25 10:26:41 +0300

    Basic Queue Operations.java

---
+++ src/Exercise_1_StacksAndQueues/P04_BasicQueueOperations.java
@@ -0,0 +1,28 @@
+package Exercise_1_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class P04_BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<Integer> number = new ArrayDeque<>();
+        int elementToPush = scanner.nextInt();
+        int elementToPop = scanner.nextInt();
+        int elementToCheck = scanner.nextInt();
+
+        for (int i = 0; i < elementToPush; i++) {
+            number.offer(scanner.nextInt());
+        }
+        for (int i = 0; i < elementToPop; i++) {
+            number.poll();
+        }
+        if (number.size() == 0) {
+            System.out.println(0);
+        } else if (number.contains(elementToCheck)) {
+            System.out.println("true");
+        } else {
+            System.out.println(number.stream().min(Integer::compare).get());
+        }
+    }
+}
