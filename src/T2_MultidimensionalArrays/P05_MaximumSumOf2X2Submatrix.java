ffc5355cb7b4351eb0068e245000fe62112f3fe6
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-25 15:21:53 +0300

    commit

---
+++ src/T2_MultidimensionalArrays/P05_MaximumSumOf2X2Submatrix.java
@@ -0,0 +1,10 @@
+package T2_MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class P05_MaximumSumOf2X2Submatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
