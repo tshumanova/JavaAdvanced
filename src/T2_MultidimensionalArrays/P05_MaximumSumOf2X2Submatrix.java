1df931019f846fd395f259ea5231d205f7249b08
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-25 15:35:46 +0300

    Maximum Sum Of 2x2 Submatrix

--- src/T2_MultidimensionalArrays/P05_MaximumSumOf2X2Submatrix.java
+++ src/T2_MultidimensionalArrays/P05_MaximumSumOf2X2Submatrix.java
@@ -1,10 +1,36 @@
 package T2_MultidimensionalArrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P05_MaximumSumOf2X2Submatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] dimensions = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        int[][] matrix = new int[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            matrix[row] = Arrays.stream(scanner.nextLine().split(", "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
