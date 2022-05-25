39de5272fd45d9bb8b69f2afacdd68eea895ff21
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-25 13:56:30 +0300

    Intersection Of Matrices

---
+++ src/T2_MultidimensionalArrays/P03_IntersectionOfTwoMatrices.java
@@ -0,0 +1,9 @@
+package T2_MultidimensionalArrays;
+
+import java.util.Scanner;
+
+public class P03_IntersectionOfTwoMatrices {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
