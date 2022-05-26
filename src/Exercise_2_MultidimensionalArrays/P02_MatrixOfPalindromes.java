7f1f90e12a2f3411b1eb36da811e33da3b92dbd5
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-26 10:59:53 +0300

    Matrix Of Palindromes

--- src/Exercise_2_MultidimensionalArrays/P02_MatrixOfPalindromes.java
+++ src/Exercise_2_MultidimensionalArrays/P02_MatrixOfPalindromes.java
@@ -13,7 +13,19 @@ public class P02_MatrixOfPalindromes {
         int cols = input[1];
 
         String[][] matrix = new String[rows][cols];
-
-
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char outside = (char) ('a' + row);
+                char middle = (char) (outside + col);
+                //matrix[row][col] = "" + outside + middle + outside;
+                matrix[row][col] = String.valueOf(outside) + middle + outside;
+            }
+        }
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
     }
 }
