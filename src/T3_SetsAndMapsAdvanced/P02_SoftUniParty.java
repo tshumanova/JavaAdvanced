bffb2a24c8cc5f9091adb32a41126a82b14e413b
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-27 09:52:34 +0300

    SoftUni Party

--- src/T3_SetsAndMapsAdvanced/P02_SoftUniParty.java
+++ src/T3_SetsAndMapsAdvanced/P02_SoftUniParty.java
@@ -3,19 +3,31 @@ package T3_SetsAndMapsAdvanced;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class P02_SoftUniParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Set<String> vip = new LinkedHashSet<>();
-        Set<String> regular = new LinkedHashSet<>();
+        Set<String> guest = new TreeSet<>();
 
         String reservationNumber = scanner.nextLine();
-        while (!reservationNumber.equals(("PARTY")) {
+        while (!reservationNumber.equals("PARTY")) {
 
 
+            guest.add(reservationNumber);
             reservationNumber = scanner.nextLine();
         }
+        String guestReservation = scanner.nextLine();
+        while (!guestReservation.equals("END")) {
+            guest.remove(guestReservation);
+
+
+            guestReservation = scanner.nextLine();
+        }
+        System.out.println(guest.size());
+
+        System.out.println(String.join(System.lineSeparator(), guest));
+
     }
 }
