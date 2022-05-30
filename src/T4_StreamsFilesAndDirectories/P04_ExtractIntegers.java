9f0c5629d1ea4ea4c788ac2b06ccf330cb7d3079
Teodora Shumanova <t.shumanova@abv.bg>
2022-05-30 13:46:26 +0300

    Extract Integers

---
+++ src/T4_StreamsFilesAndDirectories/P04_ExtractIntegers.java
@@ -0,0 +1,33 @@
+package T4_StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class P04_ExtractIntegers {
+    public static void main(String[] args) throws IOException {
+
+        String programDir = System.getProperty("user.dir");
+
+        String path = programDir + "\\input.txt";
+
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+
+        Scanner scanner = new Scanner(fileInputStream);
+
+        PrintWriter writer = new PrintWriter(new FileOutputStream("integers-out.txt"));
+
+        while (scanner.hasNext()) {
+            if (scanner.hasNextInt()) {
+
+                writer.println(scanner.next());
+            } else {
+                scanner.next();
+            }
+        }
+        writer.close();
+    }
+}
