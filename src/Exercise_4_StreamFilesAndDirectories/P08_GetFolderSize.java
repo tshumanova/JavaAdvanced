513dbee3b8a6c0377ba5fb0d3daf423a494fe85c
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-01 11:39:17 +0300

    Get Folder Size

---
+++ src/Exercise_4_StreamFilesAndDirectories/P08_GetFolderSize.java
@@ -0,0 +1,13 @@
+package Exercise_4_StreamFilesAndDirectories;
+
+import java.io.File;
+
+public class P08_GetFolderSize {
+    public static void main(String[] args) {
+
+       File folder=new File("src/Resources_4_Exercise");
+       File[]arr=folder.listFiles();
+
+
+    }
+}
