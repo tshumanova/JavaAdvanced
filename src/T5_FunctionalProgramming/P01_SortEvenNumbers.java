92e24d70be988f5a5449f9bb4f075014afe81775
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-02 14:42:55 +0300

    Sort Even Numbers

---
+++ src/T5_FunctionalProgramming/P01_SortEvenNumbers.java
@@ -0,0 +1,23 @@
+package T5_FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P01_SortEvenNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).filter(i -> i % 2 != 1).collect(Collectors.toList());
+
+        System.out.println(String.join(", ", numbers.toString().replaceAll("[\\[\\]]", "")));
+
+        Collections.sort(numbers);
+
+
+    }
+}
