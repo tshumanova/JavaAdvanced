f4435dfeecdd9655d8f4431aeae6186b4bdd8e82
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-06 15:24:22 +0300

    Find Even Or Odd

--- src/T5_FunctionalProgramming/P06_FindEvensOrOdd.java
+++ src/T5_FunctionalProgramming/P06_FindEvensOrOdd.java
@@ -1,9 +1,19 @@
 package T5_FunctionalProgramming;
 
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 public class P06_FindEvensOrOdd {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String[] range = scanner.nextLine().split("\\s+");
+        int start = Integer.parseInt(range[0]);
+        int end = Integer.parseInt(range[1]);
+        String oddOrEven = scanner.nextLine();
 
-
+        IntPredicate filterByAge = "odd".equals(oddOrEven) ? i -> i % 2 != 0 : i -> i % 2 == 0;
+        IntStream.range(start, end + 1).filter(filterByAge).forEach((i -> System.out.printf("%d ", i)));
     }
 }
