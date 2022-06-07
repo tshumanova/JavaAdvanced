3b78498c97edb03cba1e6082e0d6575b9f8957b6
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-07 10:13:28 +0300

    Predicate For Names

---
+++ src/Exercise_5_FunctionalProgramming/P06_PredicateForNames.java
@@ -0,0 +1,19 @@
+package Exercise_5_FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P06_PredicateForNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nameLength = Integer.parseInt(scanner.nextLine());
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        Predicate<String> namesToPrint = s -> s.length() <= nameLength;
+        Consumer<String> print = System.out::println;
+        names.stream().filter(namesToPrint).forEach(print);
+    }
+}
