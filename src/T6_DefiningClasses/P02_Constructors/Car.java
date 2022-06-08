2713e54bc65bda6583f0c3fd44879c4ee297153a
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-08 16:01:04 +0300

    Car

---
+++ src/T6_DefiningClasses/P02_Constructors/Car.java
@@ -0,0 +1,34 @@
+package T6_DefiningClasses.P02_Constructors;
+
+public class Car {
+    private final String brand;
+    private String model = "unknown";
+    private int horsePower = -1;
+
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public Car(String brand, String model, int horsePower) {
+        this(brand);
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP.", this.getBrand(), this.getModel(), this.getHorsePower());
+    }
+}
