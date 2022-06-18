3c3de3f487586a1a3c5852e0ad567de23a9e29aa
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-18 23:19:59 +0300

    Main

---
+++ src/T07_WorkShop/Main.java
@@ -0,0 +1,15 @@
+package T07_WorkShop;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> integers = new ArrayList<>();
+        SmartArray smartArray = new SmartArray();
+        smartArray.add(13);
+        smartArray.add(69);
+        System.out.println();
+    }
+}
