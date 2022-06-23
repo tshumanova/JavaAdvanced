6c676a6cd65e3d5974c6dae361fec92e4f36d820
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-23 14:18:50 +0300

    Flower Wreaths

---
+++ src/ExamLab/FlowerWreaths.java
@@ -0,0 +1,10 @@
+package ExamLab;
+
+import java.util.Scanner;
+
+public class FlowerWreaths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
