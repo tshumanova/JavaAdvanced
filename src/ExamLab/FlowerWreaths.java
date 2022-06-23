420824d20f775dce7c872bd5b8307c1966880fc6
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-23 14:27:43 +0300

    Flower Wreaths

--- src/ExamLab/FlowerWreaths.java
+++ src/ExamLab/FlowerWreaths.java
@@ -1,10 +1,26 @@
 package ExamLab;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FlowerWreaths {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] liliesInput = scanner.nextLine().split(", ");
+        String[] rosesInput = scanner.nextLine().split(", ");
+
+
+        List<Integer> lilies = Arrays.stream(liliesInput)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        List<Integer> roses = Arrays.stream(rosesInput)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
 
     }
 }
