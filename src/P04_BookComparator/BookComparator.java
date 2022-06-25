939dd222cfda8ab106f472470cd75b62a8bdb3ef
Teodora Shumanova <t.shumanova@abv.bg>
2022-06-25 08:28:24 +0300

    Book Comparator

---
+++ src/P04_BookComparator/BookComparator.java
@@ -0,0 +1,10 @@
+package P04_BookComparator;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book first, Book second) {
+        return first.compareTo(second);
+    }
+}
