966dd076c50cbfe659b9fdb43b7ac78ec57c690e
Teodora Shumanova <t.shumanova@abv.bg>
2022-07-07 13:02:18 +0300

    Season

---
+++ src/Java_OOP/T1_WorkingWithAbstraction/P04_HotelReservation/Season.java
@@ -0,0 +1,18 @@
+package Java_OOP.T1_WorkingWithAbstraction.P04_HotelReservation;
+
+public enum Season {
+    Autumn(1),
+    Spring(2),
+    Winter(3),
+    Summer(4);
+
+    private int multiplayer;
+
+    Season(int multiplayer) {
+        this.multiplayer = multiplayer;
+    }
+
+    public int getMultiplayer() {
+        return multiplayer;
+    }
+}
