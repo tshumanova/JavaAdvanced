17aab7c0989eb0ae9af601f72e046d5c78f0332f
Teodora Shumanova <t.shumanova@abv.bg>
2022-07-07 13:02:07 +0300

    Price Calculator

---
+++ src/Java_OOP/T1_WorkingWithAbstraction/P04_HotelReservation/PriceCalculator.java
@@ -0,0 +1,10 @@
+package Java_OOP.T1_WorkingWithAbstraction.P04_HotelReservation;
+
+public class PriceCalculator {
+    public static double priceCalculate(double pricePerDay, int numberOfDays, Season season, DiscountType discountType) {
+        double finalPrice = pricePerDay * numberOfDays;
+        finalPrice *= season.getMultiplayer();
+        finalPrice *= 1 - discountType.getPercentage() / 100.0;
+        return finalPrice;
+    }
+}
