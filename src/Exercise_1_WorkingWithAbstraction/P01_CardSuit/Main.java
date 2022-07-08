431638ea10b5acd00f0b5fe7a3fa496724807714
Teodora Shumanova <t.shumanova@abv.bg>
2022-07-08 16:14:59 +0300

    main

--- src/Exercise_1_WorkingWithAbstraction/P01_CardSuit/Main.java
+++ src/Exercise_1_WorkingWithAbstraction/P01_CardSuit/Main.java
@@ -1,7 +1,9 @@
 package Exercise_1_WorkingWithAbstraction.P01_CardSuit;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
