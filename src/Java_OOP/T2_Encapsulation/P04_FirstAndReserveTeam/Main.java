835eaad5cdc59ceaa607d84f7a80be33c9c7a40d
Teodora Shumanova <t.shumanova@abv.bg>
2022-07-14 15:27:56 +0300

    Create First And ReserveTeam

---
+++ src/Java_OOP/T2_Encapsulation/P04_FirstAndReserveTeam/Main.java
@@ -0,0 +1,27 @@
+package Java_OOP.T2_Encapsulation.P04_FirstAndReserveTeam;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int numberOfPeople = Integer.parseInt(console.nextLine());
+        Team team = new Team("Black eagles");
+
+        while (numberOfPeople-- > 0) {
+            String[] input = console.nextLine().split("\\s+");
+
+            String firstName = input[0];
+            String lastName = input[1];
+            int age = Integer.parseInt(input[2]);
+            double salary = Double.parseDouble(input[3]);
+
+            Person playerToAdd = new Person(firstName, lastName, age, salary);
+            team.addPlayer(playerToAdd);
+        }
+
+        System.out.println("First team have " + team.getFirstTeam().size() + " players");
+        System.out.println("Reserve team have " + team.getReserveTeam().size() + " players");
+    }
+}
+
