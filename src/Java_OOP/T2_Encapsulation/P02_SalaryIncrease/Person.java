5efa4b545f87a667e988f218ce61d0ef354f1fbd
Teodora Shumanova <t.shumanova@abv.bg>
2022-07-14 15:15:41 +0300

    Create Salary Increase

---
+++ src/Java_OOP/T2_Encapsulation/P02_SalaryIncrease/Person.java
@@ -0,0 +1,40 @@
+package Java_OOP.T2_Encapsulation.P02_SalaryIncrease;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.age < 30) {
+            bonus /= 2;
+        }
+
+        this.salary = this.salary * (1 + bonus / 100);
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return getFullName() + " gets " + salary + " leva";
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
